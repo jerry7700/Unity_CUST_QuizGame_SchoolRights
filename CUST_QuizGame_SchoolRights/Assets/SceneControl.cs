3ee04bbc04962bd521eddc506301ede3eaed7b48
jerry7700 <75553251+jerry7700@users.noreply.github.com>
2021-03-30 21:42:07 +0800

    介面完成

---
+++ CUST_QuizGame_SchoolRights/Assets/SceneControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("遊戲畫面");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    }
